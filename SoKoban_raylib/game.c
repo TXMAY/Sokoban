766489bdabde812bf1bd7d0cdd91690aa934dc8a
TXMAY <juya6388king@naver.com>
2021-06-19 15:20:02 +0900

    2021/06/19 히히 raylib.ver 1차 완성 히히

--- SoKoban_raylib/game.c
+++ SoKoban_raylib/game.c
@@ -1,43 +1,28 @@
 #include "raylib.h"
 
 int stage[9][9];
-char move_key;
+
 int chr_x, chr_y;
 int stage_cls = 0;
 int stone;
-void stage_print(void)
-// 0은 공백, 1은 플레이어, 2은 벽, 3는 상자, 4은 벽, 
-{
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-        {
-            if (stage[i][j] == 0)
-            
-            else if (stage[i][j] == 1)
-            
-            else if (stage[i][j] == 2)
-            
-            else
-			printf("%c", stage[i][j]);
-        }
-	}
-}
+    
+// 0은 공백, 1은 플레이어, 2은 도착지, 3는 상자, 4은 벽, 
+
 void stage_set(void)
 {
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9 ; j++)
 		{
-			if (i == 0 || j == 0 || i == 9 || j == 9)
-				stage[i][j] = 3;
+			if (i == 0 || j == 0 || i == 8 || j == 8)
+				stage[i][j] = 4;
 			else
 				stage[i][j] = 0;
 		}
 	}
-	
-
-
+    stage[4][4] = 1;
+    stage[4][5] = 3;
+    stage[2][3] = 2;
 }
 
 void get_chr(void)
@@ -47,13 +32,13 @@ void get_chr(void)
 	{
 		for (int j = 0; j < 9; j++)
 		{
-			if (stage[i][j] == '0')
+			if (stage[i][j] == 1)
 			{
 				chr_x = i;
 				chr_y = j;
-				stage[i][j] = ' ';
+				stage[i][j] = 0;
 			}
-			if (stage[i][j] == '-')
+			if (stage[i][j] == 2)
 			{
 				stone++;
 			}
@@ -62,71 +47,92 @@ void get_chr(void)
 }
 void move(void)
 {
-	int x = 0, y = 0;
-	switch (move_key)
+    int x = 0, y = 0;
+	
+	
+    if(IsKeyPressed(KEY_UP)) y = -1;
+	if(IsKeyPressed(KEY_LEFT)) x = -1;
+	if(IsKeyPressed(KEY_DOWN)) y = 1;
+	if(IsKeyPressed(KEY_RIGHT)) x = 1;
+	
+	
+	if (stage[chr_x + x][chr_y + y] == 4 || stage[chr_x + x][chr_y + y] == 2)
 	{
-	case 'w':
-		x = -1;
-		break;
-	case 'a':
-		y = -1;
-		break;
-	case 's':
-		x = 1;
-		break;
-	case 'd':
-		y = 1;
-		break;
-	default:
-		break;
+		stage[chr_x][chr_y] = 1;
 	}
-	if (stage[chr_x + x][chr_y + y] == '#' || stage[chr_x + x][chr_y + y] == '-')
+	else if (stage[chr_x + x][chr_y + y] == 3)
 	{
-		stage[chr_x][chr_y] = '0';
-	}
-	else if (stage[chr_x + x][chr_y + y] == '+')
-	{
-		if (stage[chr_x + x * 2][chr_y + y * 2] == '-')
+		if (stage[chr_x + x * 2][chr_y + y * 2] == 2)
 		{
-			stage[chr_x + x * 2][chr_y + y * 2] = '+';
-			stage[chr_x + x][chr_y + y] = '0';
+			stage[chr_x + x * 2][chr_y + y * 2] = 3;
+			stage[chr_x + x][chr_y + y] = 1;
 		}
-		else if (stage[chr_x + x * 2][chr_y + y * 2] == ' ')
+		else if (stage[chr_x + x * 2][chr_y + y * 2] == 0)
 		{
-			stage[chr_x + x * 2][chr_y + y * 2] = '+';
-			stage[chr_x + x][chr_y + y] = '0';
+			stage[chr_x + x * 2][chr_y + y * 2] = 3;
+			stage[chr_x + x][chr_y + y] = 1;
 		}
-		else if (stage[chr_x + x * 2][chr_y + y * 2] == '#' || stage[chr_x + x * 2][chr_y + y * 2] == '+')
+		else if (stage[chr_x + x * 2][chr_y + y * 2] == 4 || stage[chr_x + x * 2][chr_y + y * 2] == 3)
 		{
-			stage[chr_x + x][chr_y + y] = '+';
-			stage[chr_x][chr_y] = '0';
+			stage[chr_x + x][chr_y + y] = 3;
+			stage[chr_x][chr_y] = 1;
 		}
 	}
 	else
 	{
-		stage[chr_x + x][chr_y + y] = '0';
+		stage[chr_x + x][chr_y + y] = 1;
 	}
 }
-
 int main(void)
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "SoKoban");
+	InitWindow(screenWidth, screenHeight, "SoKoban");
+	Texture2D cat = LoadTexture("images/cat.png");
+	Texture2D wall = LoadTexture("images/wall.png");
+	Texture2D wool = LoadTexture("images/wool.png");
+    Texture2D box = LoadTexture("images/box.png");
 	stage_set();
-	while (stage_cls == 0)
+	while (!WindowShouldClose())
 	{
-		stage_print();
+		BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+        
+        for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
 
+                if (stage[i][j] == 0)
+                    DrawText("null", 280.0f + 80.0f * i, 0.0f + 80.0f * j, 10, WHITE);
+				else if (stage[i][j] == 1)
+					DrawTexture(cat, 280.0f + 80.0f * i, 0.0f + 80.0f * j, WHITE);
+				else if (stage[i][j] == 2)
+					DrawTexture(box, 280.0f + 80.0f * i, 0.0f + 80.0f * j, WHITE);
+				else if (stage[i][j] == 3)
+					DrawTexture(wool, 280.0f + 80.0f * i, 0.0f + 80.0f * j, WHITE);
+				else
+					DrawTexture(wall, 280.0f + 80.0f * i, 0.0f + 80.0f * j, WHITE);
+			}
+		}
 		get_chr();
-		get_move();
 		move();
-		system("cls");
 		if (stone == 0)
 		{
 			stage_cls++;
-			printf("clear!");
+			
 		}
+        
+        EndDrawing();
 	}
+    UnloadTexture(cat);
+    UnloadTexture(wall);
+    UnloadTexture(box);
+    UnloadTexture(wool);
+    
+    CloseWindow();
+
+    return 0;
 }
